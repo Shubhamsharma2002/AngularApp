b5ca9e1b9deec895fcc297cb49724f77c76b175d
shubham sharma <shubhamtech004@gmail.com>
2024-09-24 10:28:46 +0530

    setting up the two component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddemployeeComponent } from './Component/addemployee/addemployee.component';
+import { EmployeeListComponent } from './Component/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AddemployeeComponent,EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

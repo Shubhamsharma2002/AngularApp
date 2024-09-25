795f45b14ed2e24fde972867ae524cf517a6ee49
shubham sharma <shubhamtech004@gmail.com>
2024-09-25 12:52:19 +0530

    data binding complete

---
+++ src/app/Component/data-binding/data-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+       
+  courseName : string = "Angular 18"
+  inputType = "checkbox"
+  rollNo : number = 124
+  isIndian : boolean  = true;
+  constructor(){
+    
+  }
+
+  showAlert(message:string){
+    alert(message)
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddemployeeComponent } from './Component/addemployee/addemployee.component';
 import { EmployeeListComponent } from './Component/employee-list/employee-list.component';
+import { DataBindingComponent } from './Component/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AddemployeeComponent,EmployeeListComponent],
+  imports: [RouterOutlet,DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
